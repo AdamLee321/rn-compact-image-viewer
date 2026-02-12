11531606c87fbe7cebed18d068b90e43e5416862
Adam Lee <adam@hkc.ie>
2026-02-12 16:57:55 +0000

    build: compact image viewer exported from index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export { default as FullScreenImageViewer } from './FullScreenImageViewer';
 export type { Props as FullScreenImageViewerProps } from './FullScreenImageViewer';
+
+export { default as CompactImageViewer } from './CompactImageViewer';
+export type { CompactImageViewerProps } from './CompactImageViewer';
+
 export { default as ImageItem } from './image/ImageItem';
 export * from './types';
