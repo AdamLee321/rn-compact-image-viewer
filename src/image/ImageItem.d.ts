cda17304bb6cb3ac9bd949d7c6467751ecf9ea14
Adam Lee <adam@hkc.ie>
2026-02-13 00:18:58 +0000

    refactor: missing props readded

--- src/image/ImageItem.d.ts
+++ src/image/ImageItem.d.ts
@@ -1,16 +1,25 @@
-// @ts-nocheck
 import React from 'react';
-import { ImageSource, Dimensions } from './@types';
+import { ImageSource } from '../../@types';
 
 declare type Props = {
   imageSrc: ImageSource;
+  onRequestClose: () => void;
   onZoom: (isZoomed: boolean) => void;
+  onLongPress: (image: ImageSource) => void;
+  delayLongPress: number;
+  swipeToCloseEnabled?: boolean;
   doubleTapToZoomEnabled?: boolean;
-  layout: Dimensions;
 };
 
 declare const _default: React.MemoExoticComponent<
-  ({ imageSrc, onZoom, layout }: Props) => JSX.Element
+  ({
+    imageSrc,
+    onZoom,
+    onRequestClose,
+    onLongPress,
+    delayLongPress,
+    swipeToCloseEnabled,
+  }: Props) => JSX.Element
 >;
 
 export default _default;
